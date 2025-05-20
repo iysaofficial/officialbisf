4c7d34f7ac19e9d7646c52a39fae978c7492977a
unknown <youngscientist.iysa@gmail.com>
2025-05-20 09:06:53 +0700

    ubah logo -k

--- src/components/home/OrganizedComps.js
+++ src/components/home/OrganizedComps.js
@@ -16,13 +16,27 @@ const OrganizedComps = () => {
               </div>
               <div className="organized-image image col-lg-9 col-md-8 text-lg-start text-md-start text-center">
                 <img
-                  src="./assets/images/logo/IYSA.png"
+                  src="./assets/images/logo/logoYPBI.png"
                   className=" col-6 image-iysa"
                   alt="gallery"
                 />
                 <img
                   src="./assets/images/logo/warmadewa.png"
                   className="ms-1 col-6 image-iysa"
+                  alt="gallery"
+                />
+              </div>
+            </div>
+            <div className="row Organized-by">
+              <div className="col-lg-3 col-md-4">
+                <h4 className="h_head sponser_title">SUPPORTED BY :</h4>
+                <div className="underline1 no-margin"></div>
+                <div className="underline2 no-margin"></div>
+              </div>
+              <div className="organized-image image col-lg-9 col-md-8 text-lg-start text-md-start text-center">
+                <img
+                  src="./assets/images/logo/IYSA.png"
+                  className=" col-6 image-iysa"
                   alt="gallery"
                 />
               </div>
