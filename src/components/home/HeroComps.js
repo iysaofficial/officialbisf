0938c1f737dd0fe60243cb383271f88930a258e2
Eki Iman <ekiimanjordiansyah212@gmail.com>
2026-01-22 13:20:41 +0700

    add new button Terms and Conditions

--- src/components/home/HeroComps.js
+++ src/components/home/HeroComps.js
@@ -13,19 +13,19 @@ const HeroComps = () => {
           </p>
           <br />
           <div className="row text-center mx-auto col-lg-12">
-            <a className="registration-button m-2 mx-auto" href="/Homeregist">
+            <a className="registration-button m-2 mx-auto d-flex align-items-center justify-content-center" href="/Homeregist">
               Register Now!
             </a>
-            {/* <a className="registration-closed-button m-2 mx-auto">
-              Registration Closed
-            </a> */}
             <a
               href="https://drive.google.com/file/d/18f5vQ32q7sP-HCeob6gHjQoL8jl22Xf0/view?usp=sharing"
               target="_blank"
               rel="noreferrer"
-              className="registration-button m-2 mx-auto"
+              className="registration-button m-2 mx-auto d-flex align-items-center justify-content-center"
             >
               Guide Book
+            </a>
+            <a className="registration-button m-2 mx-auto d-flex align-items-center justify-content-center" href="https://drive.google.com/file/d/1KOtyI8EZO42INO4Q_IeiTmBQCc_8JtTl/view?usp=sharing" rel="noreferrer">
+              Terms and Conditions
             </a>
           </div>
         </div>
